1187af31ae6f6df04e467d9d077d0dbfd873b68d
waleed-bs <waleed-bs@live.com>
2018-03-19 21:33:17 -0400

    push client files

---
+++ client/components/App.jsx
@@ -0,0 +1,21 @@
+/*
+    ./client/components/App.jsx
+*/
+
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <p> test </p>
+
+      </div>
+    );
+  }
+}
+export default App;
---
+++ client/index.js
@@ -0,0 +1,13 @@
+/*
+    ./client/index.js
+    which is the webpack entry file
+*/
+/*
+    ./client/index.js
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
